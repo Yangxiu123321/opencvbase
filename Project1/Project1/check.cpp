988b3c058f074997d08b91ab8e537086d1152d91
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-02-26 11:09:43 +0800

    Houghlines

--- Project1/Project1/check.cpp
+++ Project1/Project1/check.cpp
@@ -4,22 +4,34 @@
 
 using namespace cv;
 using namespace std;
+//41.jpg
 
 int main()
 {
-	cv::Mat srcImage = cv::imread("F:\\opencvstu\\opencvbase\\Project1\\Project1\\47.png");
-	if (srcImage.empty())
-		return -1;
-	cv::imshow("srcImage ", srcImage);
-	cv::Mat grayImg1;
-	cv::cvtColor(srcImage,grayImg1,COLOR_RGB2GRAY);
-	cv::imshow("grayImg1",grayImg1);
-	std::cout << (int)grayImg1.at<uchar>(125, 100)<<endl;
+	Mat srcImg = imread("41.jpg",0);
+	namedWindow("画线图",0);
+	namedWindow("边缘检测",0);
+	vector<Vec2f> lines;
+	Mat dstImg;
+	Canny(srcImg,dstImg,50,200,3);
+	imshow("边缘检测",dstImg);
 
-	cv::Mat grayImg2;
-	cv::cvtColor(srcImage,grayImg2,COLOR_BGR2GRAY);
-	cv::imshow("grayImg2",grayImg2);
-	std::cout << (int)grayImg2.at<uchar>(125, 100);
-	cv::waitKey(0);
-	return 0;
+	HoughLines(dstImg, lines, 1, CV_PI / 180,100 );
+	Mat cdst = srcImg.clone();
+
+	for (size_t i = 0; i < lines.size(); i++)//将求得的线条画出来
+	{
+		float rho = lines[i][0], theta = lines[i][1];
+		Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cvRound(x0 + 1000 * (-b));
+		pt1.y = cvRound(y0 + 1000 * (a));
+		pt2.x = cvRound(x0 - 1000 * (-b));
+		pt2.y = cvRound(y0 - 1000 * (a));
+		line(cdst, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
+	}
+	cout << A << endl;
+	imshow("画线图", cdst);
+	waitKey(0);
 }
